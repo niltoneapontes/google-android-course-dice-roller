b01d459b7a718ed0d8bef8c449e97d7797a9a883
Nilton <niltoneapontes@gmail.com>
2023-05-29 16:52:34 -0300

    feat: finish dice logic

--- app/src/main/java/br/com/niltoneapontes/diceroller/MainActivity.kt
+++ app/src/main/java/br/com/niltoneapontes/diceroller/MainActivity.kt
@@ -3,7 +3,10 @@ package br.com.niltoneapontes.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,25 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.roll_button)
         button.setOnClickListener {
-
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val result = Random.nextInt(6) + 1
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when(result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        diceImage.setImageResource(drawableResource)
+
     }
 }
